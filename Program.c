3512697ff7a37a34cf62baf7a5e49e475eb276f0
ratikaewkam <ratikaewkam.rk@gmail.com>
2024-10-09 07:11:48 +0700

    Update Program.c - ASCII Text to Hex

--- Program.c
+++ Program.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    char source[7] = "123456";
+    int n = strlen(source);
+    int i;
+    printf("%d\n", n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("0x%X ", source[i]);
+    }
+
     return 0;
 }
