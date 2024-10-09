f666a6291e48ef79c85b0724988b064e2a823ff2
ratikaewkam <ratikaewkam.rk@gmail.com>
2024-10-09 23:45:36 +0700

    Update Program.c - Destination to AX.25 Frame

--- Program.c
+++ Program.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main()
+void outHead(char data[], int n)
 {
-    char source[7] = "123456";
-    int n = strlen(source);
     int i;
-    printf("%d\n", n);
 
     for (i = 0; i < n; i++)
     {
-        printf("0x%X ", source[i]);
+        data[i] = data[i] << 1;
+        printf("%X ", data[i]);
     }
+}
+
+int main()
+{
+    // Data
+    char source[7] = "N7LEM  ";
+    char sourceSSID[2] = "0";
+    char dest[7] = "NJ7P  ";
+    char destSSID[2] = "0";
+    int PID = 240;
+    int control = 3;
+    char payload[256] = "The quick brown fox jumps over the lazy dog";
+
+    // Length of array
+    int nSource = strlen(source);
+    int nSourceSSID = strlen(sourceSSID);
+    int nDest = strlen(dest);
+    int nDestSSID = strlen(destSSID);
+    int nPayload = strlen(payload);
+
+    // AX.25 Frame
+    outHead(dest, nDest);
+    // outHead(source, nSource);
 
     return 0;
 }
